66b2f9e71429c6e417504229d3cbe08496262b2e
Greg Stromire <gstromire@gmail.com>
2019-10-23 22:14:16 -0700

    Minor AppError handling update

--- src/main/scala/AppError.scala
+++ src/main/scala/AppError.scala
@@ -4,7 +4,7 @@ import cats.data.Kleisli
 import cats.effect.Sync
 import cats.implicits._
 import org.http4s.dsl.Http4sDsl
-import org.http4s.HttpApp
+import org.http4s.{HttpApp, Response}
 
 sealed trait AppError extends Exception with Product with Serializable
 object AppError {
@@ -13,9 +13,15 @@ object AppError {
 
 class ErrorHandler[F[_]: Sync](service: HttpApp[F]) extends Http4sDsl[F] {
 
+  private type Handler[E <: Exception with Product with Serializable] = E => F[Response[F]]
+
+  private val appErrorHandler: Handler[AppError] = {
+    case AppError.InvalidBody => BadRequest("No good")
+  }
+
   def wrapped: HttpApp[F] = Kleisli { request =>
     service(request).recoverWith {
-      case AppError.InvalidBody => BadRequest("No good")
+      case e: AppError => appErrorHandler(e)
     }
   }
 }
